936b42737239b351aecc73ea101a3241b5cfd038
carlos henrique <hmontalione@vertigo.com.br>
2018-03-27 15:54:59 -0300

    implementando rede-lista referenciado

--- src/app/referenciado/rede-lista/rede-lista.component.ts
+++ src/app/referenciado/rede-lista/rede-lista.component.ts
@@ -1,8 +1,9 @@
 import { ReferenciadoLocalizacao } from './../../shared/referenciadoLocalizacao.model';
 import { ReferenciadoEspecialidade } from '../../shared/referenciadoEspecialidade.model';
 import { ReferenciadoService } from './../referenciado.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Referenciado } from '../../shared/referenciado.model';
+import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-rede-lista',
@@ -10,24 +11,26 @@ import { Referenciado } from '../../shared/referenciado.model';
 })
 export class RedeListaComponent implements OnInit {
 
-  referenciadoEspecialidade: ReferenciadoEspecialidade;
-  listaEspecialidade: Array<ReferenciadoEspecialidade>;
+  private referenciadoEspecialidade: ReferenciadoEspecialidade;
+  private listaEspecialidade: Array<ReferenciadoEspecialidade>;
 
-  referenciadoLocalizacao: ReferenciadoLocalizacao;
-  listaLocalizacao: Array<ReferenciadoLocalizacao>;
+  private referenciadoLocalizacao: ReferenciadoLocalizacao;
+  private listaLocalizacao: Array<ReferenciadoLocalizacao>;
 
-  referenciado: Referenciado;
-  listaReferenciado: Array<Referenciado>;
+  private referenciado: Referenciado;
+  private listaReferenciado: Array<Referenciado>;
 
-  localizacao: string;
-  especialidadeId: string = '';
-  bairro: string = '';
-
+  private localizacao: string;
+  private especialidadeId = '0';
+  private bairro = '';
+  private planoID = '';
+  private cidade = '';
   private mensagemErro: string;
 
-  constructor(private referenciadoService: ReferenciadoService) {}
+  constructor(private referenciadoService: ReferenciadoService, @Inject(LOCAL_STORAGE) private storage: WebStorageService) {}
 
   ngOnInit() {
+    this.setStorage();
 
     this.referenciadoService.getEspecialidade().
       subscribe(referenciadoEspecialidade => {
@@ -40,7 +43,19 @@ export class RedeListaComponent implements OnInit {
         this.mensagemErro = 'O serviço de Especialidade está fora do ar.';
       });
       
-      this.referenciadoService.getLocalizacao().
+      this.referenciadoService.getLocalizacao(this.planoID, this.cidade, this.especialidadeId).
+      subscribe(referenciadoLocalizacao => {
+        this.listaLocalizacao = referenciadoLocalizacao;
+        console.log(this.listaLocalizacao);
+        console.log('bairro: ' + this.listaLocalizacao[0].bairro);
+      },
+      error => {
+        this.mensagemErro = 'O serviço de Localização está fora do ar.';
+      });
+  }
+
+  pesquisar() {
+    this.referenciadoService.getLocalizacao(this.planoID, this.cidade, this.especialidadeId).
       subscribe(referenciadoLocalizacao => {
         this.listaLocalizacao = referenciadoLocalizacao;
         console.log(this.listaLocalizacao);
@@ -51,15 +66,9 @@ export class RedeListaComponent implements OnInit {
       });
 
 
-  }
 
-  pesquisar() {
 
-    if (this.especialidadeId == undefined) return;
-
-    console.log('Especialidade : ' + this.especialidadeId);
-
-    this.referenciadoService.getReferenciado(this.especialidadeId, this.bairro).
+    this.referenciadoService.getReferenciado(this.especialidadeId, this.bairro, this.planoID, this.cidade).
     subscribe(referenciado => {
       this.listaReferenciado = referenciado;
       console.log(this.listaReferenciado);
@@ -68,9 +77,14 @@ export class RedeListaComponent implements OnInit {
     error => {
       this.mensagemErro = 'O serviço de Referenciado está fora do ar.';
     });
-
   }
 
+  setStorage() {
+    this.storage.set('planoId', '5');
+    this.storage.set('cidade', 'rio de janeiro');
 
-
+    this.cidade = this.storage.get('cidade');
+    this.planoID = this.storage.get('planoId');
+    console.log('planoId: ', this.storage.get('planoId'), ', cidade: ', this.storage.get('cidade'));
+  }
 }
